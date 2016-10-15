ec906578f6023885da1e34fd1456e69ab23a7ae3
Andrey Toshchev <toschev.andrey@andex.ru>
2016-10-15 20:55:41 +0300

    Local Host: Step 0 - Client - isAuthentication

--- The final project/client.py
+++ The final project/client.py
@@ -1,51 +1,82 @@
 import sys
 import socket
+import select
 
-if len(sys.argv) != 3:
-    print("Usage: python chatclient.py [host] [port number")
-    sys.exit()
+def isAuthentication(sock):
+    try:
+        isOkay = False;
+        register = input('Sign up or Login? ')
+        sign = ''
+        if 'sign' in register.lower():
+            sign = 'Sign '
+        else:
+            sign = 'login '
 
-host = sys.argv[1]
-port = int(sys.argv[2])
-print('Welcome to the socketChat client.')
+        while(isOkay == False):
+            login = input('Choose your nameUsers: ')
+            password = input('Your password: ')
 
-# get nameUsers
-nameUsers = input('Choose your nameUsers: ')
+            #string = sign + login+'+'+password
+            #sock.send(bytes(string +'\n', 'cp866'))
+            string = sign + login+'+'+password + '\n'
+            sock.send(string.encode())
+            print(1)
+            response = sock.recv(512).decode('cp866')
+            print("response: "+response)
+            print(1)
+            if 'login ok' in response:
+                isOkay = True
+            else:
+                print('Wrong input login and password')
+        return login;
+    except:
+        print('Authentication problems ')
+        sys.exit()
 
-# create socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def chat_client():
+    if len(sys.argv) != 3:
+        print("Usage: python chatclient.py [host] [port number")
+        sys.exit()
 
-try:
-    # get host and connect socket
-    host_ip = socket.gethostbyname(host)
-    sock.connect((host_ip, port))
-    #sock.send(bytes("Hi! Starting!\n", 'UTF-8'))
-    msg = input("%s> " % nameUsers)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    print('Welcome to the socketChat client.')
 
-    while (True):#(msg != 'quit'):
-        while len(msg) > 1024:
-            print('Your message was too long. Maximum 1024 characters.')
-            msg = input("%s> " % nameUsers)
-        #sock.send("b%s> %s" % (nameUsers,msg))
-        #sock.send(msg.encode()) # work
-        sock.send(bytes(msg+'\n', 'cp866')) # work
-            
-        print('[waiting for response...]')
-        response = sock.recv(1024).decode('cp866')
-        if 'quit' in response:
-            print("Received quit signal. Closing connection.")
-            sys.exit()
-        print(response)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    try:
+        host_ip = socket.gethostbyname(host)
+        sock.connect((host_ip, port))
+
+        nameUsers = isAuthentication(sock)
+        print(2)
         msg = input("%s> " % nameUsers)
-    
-    # react to quit message
-    print("Closing Connection.")
-    # send quit to server
-    sock.send(bytes('quit\n', 'cp866'))
-except socket.gaierror:
-    print('Could not find host. Exiting.')
-    sys.exit()
-finally:
-    print(sys.stderr, 'closing socket')
-    sock.close()
+
+        while ('quit' in msg) == False:
+            while len(msg) > 1024:
+                print('Your message was too long. Maximum 1024 characters.')
+                msg = input("%s> " % nameUsers)
+            #sock.send(msg.encode())
+            sock.send(bytes(msg+'\n', 'cp866'))
+                
+            print('[waiting for response...]')
+            response = sock.recv(1024).decode('cp866')
+            if 'quit' in response:
+                print("Received quit signal. Closing connection.")
+                sys.exit()
+            print(response)
+
+            msg = input("%s> " % nameUsers)
+        
+        print("Closing Connection.")
+
+        sock.send(bytes('quit\n', 'cp866'))
+    except socket.gaierror:
+        print('Could not find host. Exiting.')
+        sys.exit()
+    finally:
+        print(sys.stderr, 'closing socket')
+        sock.close()
+
+if __name__ == "__main__":
+    sys.exit(chat_client())
